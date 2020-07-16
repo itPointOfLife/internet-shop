31a4db8b3eaaed320c8c7365dd02070b3e3aa2a0
itPointOfLife <it_original@ukr.net>
2020-07-16 15:00:48 +0300

    Updates

---
+++ precache-manifest.a271f327590d3318ff1c037be89a25a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6016f8a1d4d02f8fc2d7ab81a3d63f",
+    "url": "/internet-shop/index.html"
+  },
+  {
+    "revision": "8de39d3260b7e68b61d7",
+    "url": "/internet-shop/static/css/main.dfd3fb1c.chunk.css"
+  },
+  {
+    "revision": "0918744903767f3b7d06",
+    "url": "/internet-shop/static/js/2.2e3e41a7.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/internet-shop/static/js/2.2e3e41a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de39d3260b7e68b61d7",
+    "url": "/internet-shop/static/js/main.fb8a138b.chunk.js"
+  },
+  {
+    "revision": "d0537bc181474d92f7d4",
+    "url": "/internet-shop/static/js/runtime-main.d1748a75.js"
+  }
+]);
